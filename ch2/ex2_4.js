e2d7b70e4a4c7f994eddce0678b38653d98e6745
subin001002 <subin001002@gmail.com>
2021-08-09 16:26:44 +0900

    master

---
+++ ch2/ex2_4.js
@@ -0,0 +1,23 @@
+function add1(x, y) {
+    return x + y;
+  }
+  
+const add2 = (x, y) => {
+return x + y;
+};
+
+const add3 = (x, y) => x + y;
+
+const add4 = (x, y) => (x + y);
+
+//add1 = add2 = add3 = add4
+
+function not1(x) {
+return !x;
+}
+
+const not2 = x => !x;
+
+//not1 = not2
+
+  
