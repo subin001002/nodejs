31f4e0aa24d3abde5d627d3cdea55a112f606b14
subin001002 <subin001002@gmail.com>
2021-08-14 21:03:20 +0900

    master

---
+++ ch6/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+  console.log('1 요청에 실행하고싶어요');
+  next();
+}, (req, res, next) => {
+  throw new Error('에러가 났어요');
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.post('/', (req, res) => {
+   res.send('Hello, Express!');
+});
+app.get('/about', (req, res) => {
+  res.send('Hello, Express');
+});
+app.get('*', (req, res) => {
+  res.send('Hello, Everybody');
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send('에러남');
+})
+app.listen(app.get('port'), () => {
+  console.log('익스프레스 서버 실행');
+});
